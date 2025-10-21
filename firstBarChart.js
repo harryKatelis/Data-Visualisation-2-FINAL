d74b6873fdd4905420e2fce82bbde156dd9ad4e2
harryKatelis <140780709+harryKatelis@users.noreply.github.com>
2025-10-21 20:01:38 +1100

    Add files via upload

---
+++ firstBarChart.js
@@ -0,0 +1,5 @@
+var vg_1 = "firstBarChart.vg.json";
+vegaEmbed("#firstBarChart", vg_1).then(function(result) {
+// Access the Vega view instance
+//(https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
---
+++ stateBarCharts.js
@@ -0,0 +1,5 @@
+var vg_1 = "stateBarCharts.vg.json";
+vegaEmbed("#stateBarCharts", vg_1).then(function(result) {
+// Access the Vega view instance
+//(https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
